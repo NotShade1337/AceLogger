bbad5632352aee4598aee52720f111253604fe34
NotShade <157139507+NotShade1337@users.noreply.github.com>
2024-01-19 21:25:09 +0530

    Safe Update

--- post.js
+++ post.js
@@ -1,4 +1,4 @@
-let pfp = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.ks5ZsJ5JuPM1TzM9pIdslgHaHa%26pid%3DApi&f=1&ipt=1a9b299a37784150b97eedeba0596ce13d28f9dcb22f69e4b02a74e3cfd08471&ipo=images'
+let pfp = '{PFP LINK/SOURCE}'
 let dto = new Date
 let dt = dto.toLocaleDateString()
 
@@ -67,38 +67,38 @@ function arrange() {
 }
 async function post(){
     const res = await fetch(
-        'https://discord.com/api/webhooks/1197852550536384603/JfRVfttj7KYjTd_JD1ylxyCqIYQFRuwsxE0oZp9_-bhwYFdIm7lcWTkGKM5jg2r4r8VD?thread_id=1196461571950514286',
+        webhook_api_With_token_and_threads_if_required,
         {
           method: 'post',
           headers: {
             'Content-Type': 'application/json',
           },
           body: await JSON.stringify({
-            username: 'Portgas D. Ace',
+            username: '{Name}',
             avatar_url: pfp,
             content:
               `# ${dt}`,
             embeds: [
               {
                 
-                color: 16742912,
+                color: {color},
                 author: {
                   name: 'LOGGER',
-                  url: 'https://www.youtube.com/watch?v=SpZ2FsEfwP4',
+                  url: 'url',
                   icon_url: pfp,
                 },
                 title: tie,
                 url:
-                  'https://aniwatch.to/one-piece-100',
+                  '{url}';,
                 description: `${DiaryStuffLongPARA}`,
                 fields: [
                   {
-                    name: 'Major Stuff Done Today:',
+                    name: '{field_name}:',
                     value: bp.join("\n"),
                   }
                 ],
                 footer: {
-                  text: 'All The Stars Shine For You',
+                  text: '{footer_text}',
                 },
               },
             ],
